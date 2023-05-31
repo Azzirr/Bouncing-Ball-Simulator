3a105b48c82b1cc54750b2c5f4a8332ff677cbf6
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-05-31 15:50:14 +0200

    Some logic

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,7 +4,7 @@ export default function Board(){
     const [board, setBoard] = useState([
         ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
         ['X', '1', '0', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-        ['X', '0', 'X', '0', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+        ['X', '0', '0', '0', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
         ['X', '0', '0', 'X', '0', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
         ['X', '0', '0', '0', '0', '0', 'X', 'X', 'X', 'X', 'X', 'X'],
         ['X', '0', '0', '0', '0', '0', '0', 'X', 'X', 'X', 'X', 'X'],
@@ -20,9 +20,382 @@ export default function Board(){
         ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
       ]);
     const toRepeat = board[1].length;
-    const borderPositions = [];
+    // TO DO! FOR LOOP THAT MAKING THIS ARRAY FOR ME!!!
+    const borderPositions = [
+      [
+          0,
+          0
+      ],
+      [
+          0,
+          1
+      ],
+      [
+          0,
+          2
+      ],
+      [
+          0,
+          3
+      ],
+      [
+          0,
+          4
+      ],
+      [
+          0,
+          5
+      ],
+      [
+          0,
+          6
+      ],
+      [
+          0,
+          7
+      ],
+      [
+          0,
+          8
+      ],
+      [
+          0,
+          9
+      ],
+      [
+          0,
+          10
+      ],
+      [
+          0,
+          11
+      ],
+      [
+          1,
+          0
+      ],
+      [
+          1,
+          3
+      ],
+      [
+          1,
+          4
+      ],
+      [
+          1,
+          5
+      ],
+      [
+          1,
+          6
+      ],
+      [
+          1,
+          7
+      ],
+      [
+          1,
+          8
+      ],
+      [
+          1,
+          9
+      ],
+      [
+          1,
+          10
+      ],
+      [
+          1,
+          11
+      ],
+      [
+          2,
+          0
+      ],
+      [
+          2,
+          2
+      ],
+      [
+          2,
+          4
+      ],
+      [
+          2,
+          5
+      ],
+      [
+          2,
+          6
+      ],
+      [
+          2,
+          7
+      ],
+      [
+          2,
+          8
+      ],
+      [
+          2,
+          9
+      ],
+      [
+          2,
+          10
+      ],
+      [
+          2,
+          11
+      ],
+      [
+          3,
+          0
+      ],
+      [
+          3,
+          3
+      ],
+      [
+          3,
+          5
+      ],
+      [
+          3,
+          6
+      ],
+      [
+          3,
+          7
+      ],
+      [
+          3,
+          8
+      ],
+      [
+          3,
+          9
+      ],
+      [
+          3,
+          10
+      ],
+      [
+          3,
+          11
+      ],
+      [
+          4,
+          0
+      ],
+      [
+          4,
+          6
+      ],
+      [
+          4,
+          7
+      ],
+      [
+          4,
+          8
+      ],
+      [
+          4,
+          9
+      ],
+      [
+          4,
+          10
+      ],
+      [
+          4,
+          11
+      ],
+      [
+          5,
+          0
+      ],
+      [
+          5,
+          7
+      ],
+      [
+          5,
+          8
+      ],
+      [
+          5,
+          9
+      ],
+      [
+          5,
+          10
+      ],
+      [
+          5,
+          11
+      ],
+      [
+          6,
+          0
+      ],
+      [
+          6,
+          8
+      ],
+      [
+          6,
+          9
+      ],
+      [
+          6,
+          10
+      ],
+      [
+          6,
+          11
+      ],
+      [
+          7,
+          0
+      ],
+      [
+          7,
+          11
+      ],
+      [
+          8,
+          0
+      ],
+      [
+          8,
+          11
+      ],
+      [
+          9,
+          0
+      ],
+      [
+          9,
+          4
+      ],
+      [
+          9,
+          11
+      ],
+      [
+          10,
+          0
+      ],
+      [
+          10,
+          3
+      ],
+      [
+          10,
+          4
+      ],
+      [
+          10,
+          5
+      ],
+      [
+          10,
+          11
+      ],
+      [
+          11,
+          0
+      ],
+      [
+          11,
+          4
+      ],
+      [
+          11,
+          11
+      ],
+      [
+          12,
+          0
+      ],
+      [
+          12,
+          11
+      ],
+      [
+          13,
+          0
+      ],
+      [
+          13,
+          11
+      ],
+      [
+          14,
+          0
+      ],
+      [
+          14,
+          11
+      ],
+      [
+          15,
+          0
+      ],
+      [
+          15,
+          1
+      ],
+      [
+          15,
+          2
+      ],
+      [
+          15,
+          3
+      ],
+      [
+          15,
+          4
+      ],
+      [
+          15,
+          5
+      ],
+      [
+          15,
+          6
+      ],
+      [
+          15,
+          7
+      ],
+      [
+          15,
+          8
+      ],
+      [
+          15,
+          9
+      ],
+      [
+          15,
+          10
+      ],
+      [
+          15,
+          11
+      ]
+  ];
+
+    const [vector, setVector] = useState('rightDown')
 
     const [currentPosition, setCurrentPosition] = useState();
+    const [collision, setCollision] = useState(false);
 
     function findCurrentPosition(){
       setCurrentPosition(() => {
@@ -35,32 +408,57 @@ export default function Board(){
         }
       })
     }
-    function findXPositions(){
-      for(let i = 0; i < board.length; i++){
-        for(let j = 0; j < board[i].length; j++){
-          if(board[i][j].includes('X')){
-            borderPositions.push([i, j])
-          }
-        }
-      }
+    // function findXPositions(){
+    //   for(let i = 0; i < board.length; i++){
+    //     for(let j = 0; j < board[i].length; j++){
+    //       if(board[i][j].includes('X')){
+    //         borderPositions.push([i, j])
+    //       }
+    //     }
+    //   }
+    // }
+    function run(){
+      findCurrentPosition();
+      changeCoordinates();
     }
-    function test(){
-      console.log(borderPositions)
+    function randomVector(){
+      if(vector === 'rightDown'){
+        return setVector('leftDown');
+      } else if(vector === 'leftDown'){
+        return setVector('leftTop');
+      } else if(vector === 'leftTop'){
+        return setVector('rightTop');
+      } else if(vector === 'rightTop'){
+        return setVector('rightDown');
+      }
     }
 
     function changeCoordinates(){
       let newBoard = board;
 
       for(let i = 0; i < borderPositions.length; i++){
-          if([currentPosition[0] + 1, currentPosition[1] + 1] === borderPositions[i]){
-            console.log('dupa')
+          if(currentPosition[0] === borderPositions[i][0] && currentPosition[1] === borderPositions[i][1]){
+            setCollision(true);
           }
       }
-
-      newBoard[currentPosition[0]][currentPosition[1]] = '0';
-      newBoard[currentPosition[0] + 1][currentPosition[1] + 1] = '1';
-      setBoard(newBoard)
+      console.log(board[currentPosition[0]][currentPosition[1]])
+      console.log(currentPosition)
+      if(newBoard[currentPosition[0]][currentPosition[1]] === '1' && vector === 'rightDown'){
+        newBoard[currentPosition[0]][currentPosition[1]] = '0';
+        newBoard[currentPosition[0] + 1][currentPosition[1] + 1] = '1';
+        setBoard(newBoard)
+        if(collision === true){
+          setCollision(false);
+          randomVector();
+        }
+      } else if(newBoard[currentPosition[0]][currentPosition[1]] === '1' && vector === 'leftDown'){
+        newBoard[currentPosition[0]][currentPosition[1]] = '0';
+        newBoard[currentPosition[0] + 1][currentPosition[1] - 1] = '1';
+        setBoard(newBoard)
+      }
     }
+
+
       return(
         <>
         <div style={{
@@ -76,8 +474,7 @@ export default function Board(){
         </div>
         <button onClick={findCurrentPosition}>FIND</button>
         <button onClick={changeCoordinates}>CHANGE</button>
-        <button onClick={findXPositions}>TEST</button>
-        <button onClick={test}>TEST</button>
+        <button onClick={run}>RUN</button>
         </>
       )
 }
