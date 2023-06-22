e839fc137190ba649e8625337c9b3b451e8620f0
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-06-22 20:34:35 +0200

    Delete test button

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -95,7 +95,6 @@ export default function Board(){
         }
         </div>
         <button onClick={run}>RUN</button>
-        <button onClick={randomizeDirection}>Randomize</button>
         </>
       )
 }
